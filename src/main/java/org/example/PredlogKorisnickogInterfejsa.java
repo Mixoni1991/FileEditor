c9ad68968e7b8cb99d5c7a7c8886e5214d735e78
Mixoni1991 <pantelic.mihajlo@gmail.com>
2023-05-24 08:19:45 +0200

    no mmsg

---
+++ src/main/java/org/example/PredlogKorisnickogInterfejsa.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class PredlogKorisnickogInterfejsa extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonClose;
+    private JButton buttonSave;
+    private JButton buttonOpen;
+    private JButton buttonGetSelection;
+    private JTextArea textAreaTop;
+    private JTextArea textAreaBottom;
+    private JTextArea textAreaNew;
+
+    public PredlogKorisnickogInterfejsa() {
+        setContentPane(contentPane);
+        setModal(true);
+        //    getRootPane().setDefaultButton(buttonOpen);
+
+        buttonClose.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onButtonClose();
+            }
+        });
+
+
+        // call onCancel() when cross is clicked
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onButtonClose();
+            }
+        });
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+    }
+
+    private void onButtonClose() {
+        // add your code here if necessary
+        dispose();
+    }
+    public static void main(String[] args) {
+        PredlogKorisnickogInterfejsa dialog = new PredlogKorisnickogInterfejsa();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
